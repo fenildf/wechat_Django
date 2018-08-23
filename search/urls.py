e3baf7a37a1d8ab50bb2b456cd89e06ca5e88bc1
pfish <perry1998@outlook.com>
2018-08-23 21:03:30 +0800

    第一次测试版，上传服务器

--- search/urls.py
+++ search/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 
 app_name = 'search'
 urlpatterns = [
+
     path('grade/<str:sid>/<str:pwd>/', views.grade, name='grade'),
     path('table/<str:sid>/<str:pwd>/', views.time_table, name='time_table')
 ]
